8557137bbcf5c589b08eb4413bcfa60abd91b1ea
scul0405 <90521402+scul0405@users.noreply.github.com>
2022-08-11 13:40:01 +0700

    update eslint and prettier

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const dotenv = require('dotenv');
-dotenv.config({"path" : "./.env"});
+
+dotenv.config({ path: './.env' });
 const express = require('express');
-const server = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
 const helmet = require('helmet');
 const hpp = require('hpp');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
-const globalErrorHandler = require('./controllers/errorController');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/AppError');
 
-
+const server = express();
 const port = process.env.PORT || 3000;
 
 // SET SECURITY HTTP HEADER
@@ -26,48 +27,50 @@ server.use(xss());
 
 // Prevent parameters pollution
 server.use(
-    hpp({
-      whitelist: [
-      ],
-    })
-  );
+  hpp({
+    whitelist: [],
+  })
+);
 
 // RATE LIMIT
 const limiter = rateLimit({
-    max: process.env.MAX_RATE_LIMIT,
-    windowMs: process.env.MAX_RATE_LIMIT_TIME * 60 * 1000, // unit: minutes
-    message:
-      `Too many requests from this IP, please try again after ${process.env.MAX_RATE_LIMIT_TIME} minutes !`,
-  });
-  
+  max: process.env.MAX_RATE_LIMIT,
+  windowMs: process.env.MAX_RATE_LIMIT_TIME * 60 * 1000, // unit: minutes
+  message: `Too many requests from this IP, please try again after ${process.env.MAX_RATE_LIMIT_TIME} minutes !`,
+});
+
 server.use('/api', limiter);
 
 // CORS
 server.use(cors());
 
 // Morgan
-if (process.env.NODE_ENV === 'development') 
-    server.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') server.use(morgan('dev'));
 
 server.use(express.json());
 
 // Handle when no match any routes
-server.all('*', (req,res,next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server !`, 404))
-})
+server.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server !`, 404));
+});
 
 // Execute all errors throw out global
 server.use(globalErrorHandler);
 
 // Database connection
-const DB_URI = process.env.MONGODB_URI.replace('<password>',process.env.MONGODB_PASSWORD);
+const DB_URI = process.env.MONGODB_URI.replace(
+  '<password>',
+  process.env.MONGODB_PASSWORD
+);
 
-mongoose.connect(DB_URI,{
-    useNewUrlParser: true
-}).then(() => {
-    console.log('Connect to database successful !')
-});
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Connect to database successful !');
+  });
 
-server.listen(port,() => {
-    console.log(`Server running on port ${port}`)
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
